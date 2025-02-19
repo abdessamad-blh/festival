7ce2a682b0658948e3227a7c97c8db4892b2788c
abdessamad-blh <abdoblh778@gmail.com>
2025-02-19 16:58:38 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import ('tailwindcss').Config} */
+export default {
+    content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Assure-toi que tous les fichiers dans src sont scannés
+    ],
+    theme : {
+        extend:{
+            fontFamily:{
+                inter:["Inter","sans-serif"],
+            },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(circle, rgba(255,255,255,0) 0%, rgba(0,0,0,1) 100%)",
+            },
+        },
+        screens:{
+            // => @media (max-width:1200px){...}
+            lg:{min: "925px"},
+        },
+    },
+    plugins:[],
+}
